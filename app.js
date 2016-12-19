c8aa3a582141cf40339b5105e6ed5f65218d470c
Anna Kimtis <Anna@Annas-MBP.verizon.net>
2016-12-19 13:30:51 -0500

    Added functional buttons prepended to the page

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 
 
 
-var topics = ["orchid", "rose", "lilly", "foxglove", "peonie", "lilac"]
+var topics = ["squares", "swirls", "mandelbrot", "circles", "angles"]
 
-for ( var i= 0; i< topics.length; i++) {
-$(#buttons).append("<button type="button" class="btn btn-primary" data-search="topics[i]">"+topics[i]+"</button>");
+for (var i= 0; i< topics.length; i++) {
+  var buttonHTML = '<button type="button" class="btn btn-primary buttonSearch" data-search=' + '"' + topics[i] + '" >'+  topics[i] +'</button>';
+  $("#buttons").append(buttonHTML);
 }
     
 
-$(.button).on("click", function()){
-  var  = $(this).data("search")
+$(".buttonSearch").on("click", function(){
+  var x = $(this).data("search")
+ 
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q="+x+"&api_key=dc6zaTOxFJmzC&limit=10&rating=G";
+  console.log(queryURL)
 
-var queryURL = "http:api.giphy.com/v1/gifs/search?q="+x+"&api_key=dc6zaTOxFJmzC&limit=10";
-console.log(queryURL)
-
-$.ajax({url: queryURL,
+  $.ajax({url: queryURL,
       method: 'GET'
     }).done(function(response) {
       console.log(response);
-      $('body').append(<p>Rating: "+response.data[0].rating+" "</p>");
-      $('body').append("<img src='"+response.data[0].images.downsized.url+"'>")
-    });
+      console.log(response.data);
+      console.log(response.data[0]);
+      for (var j=0; j<response.data.length; j++){
+        $('#gifArea').prepend('<p>Rating: ' + response.data[j].rating + '</p>');
+        $('#gifArea').prepend('<img src=' +'"'+ response.data[j].images.downsized.url +'"'+ '>')
+      }
+  });
 
-}
+});
